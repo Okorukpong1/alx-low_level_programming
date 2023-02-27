b7c5ff0489e5d223d2b8205bb3a38940b60c342a
AdetunjiDanielOlamide <adetunjidanielolamide@gmail.com>
2023-02-27 03:05:31 -0800

    My C-pas

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - a function that takes a pointer to an int as parameter
- * and updates the values it points to 98
- * @n: input
+ * reset_to_98 - writes a function that points to an int and updates
+ *               the value of the address to 98.
+ * @n: The int declared varialble name
  */
 
 void reset_to_98(int *n)
