06d060d8eb097a300760d6eba9435f7f399215be
AdetunjiDanielOlamide <adetunjidanielolamide@gmail.com>
2023-02-27 02:59:44 -0800

    My C-pas

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,12 @@
+#include "main.h"
 
+/**
+ * main - a function that takes a pointer to an int as parameter
+ * and updates the values it points to 98
+ * @n: input
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
