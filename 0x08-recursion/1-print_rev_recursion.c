fc716711111e9c2ab002fd98a848efb881eb1bb9
AdetunjiDanielOlamide <adetunjidanielolamide@gmail.com>
2023-03-08 11:22:50 -0800

    Recursio

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,7 +3,6 @@
 /**
  * _print_rev_recursion - Prints a string in reverse.
  * @s: string to print
- * Return: Always 0 (Success)
  */
 
 void _print_rev_recursion(char *s)
@@ -11,7 +10,6 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		_putchar (*s);
 	}
-	return (0);
 }
