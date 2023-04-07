7942b738c5a838e0fac53a41b396e22098c903f1
AdetunjiDanielOlamide <adetunjidanielolamide@gmail.com>
2023-04-07 09:31:17 -0700

    But manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,10 +5,10 @@
  * @n: number that will be printed to binary
  */
 
-void print_binary(unsigned long int n);
+void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
 
-	_putchar((0 & 1) + '0');
+	_putchar((n & 1) + '0');
 }
